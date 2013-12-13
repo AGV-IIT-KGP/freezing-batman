478c026eb07ed43d59509da0c8f5205b88b0210e
Samuel Anudeep <banmido69@gmail.com>
2013-12-14 03:58:45 +0530

    Modified library names within the CMake for better appearence.

---
+++ agv_framework/src/Environment/Interpreter.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   Interpreter.cpp
+ * Author: samuel
+ * 
+ * Created on 14 December, 2013, 12:50 AM
+ */
+
+#include "Environment/Interpreter.hpp"
+
+namespace environment {
+
+    void Interpreter::interpret() {
+
+    }
+}
+
---
+++ agv_framework/src/Environment/Model.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Model.cpp
+ * Author: samuel
+ * 
+ * Created on 14 December, 2013, 1:21 AM
+ */
+
+#include "Environment/Model.hpp"
+
+namespace environment {
+
+    Model::Model() {
+    }
+
+    Model::Model(const Model& orig) {
+    }
+
+    Model::~Model() {
+    }
+}
+
