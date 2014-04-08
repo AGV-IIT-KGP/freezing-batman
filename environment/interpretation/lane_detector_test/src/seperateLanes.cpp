6f495f165861760e581ce3226d579da6d75d8548
Harsh Gupta <gupta.harsh96@gmail.com>
2014-04-09 02:08:38 +0530

    Added STL queue instead to own implementation

--- environment/interpretation/lane_detector_test/src/seperateLanes.cpp
+++ environment/interpretation/lane_detector_test/src/seperateLanes.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 #include <cmath>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
@@ -53,101 +54,15 @@ public:
     }
 };
 
-class Curve_C{
-public:
-	Curve_C(){
-	}	
-	std::vector<double> constants;
-	int degree;
-	void make_curve(std::vector<point> &p, int degree_){
-		degree = degree_;
-		Eigen::MatrixXd m(p.size(),degree+1);
-		Eigen::MatrixXd y(p.size(),1);
-		for(int r=0;r<p.size();r++){
-			for(int c=0;c<=degree;c++){
-				m(r,c)=pow(p[r].x,c);
-				y(r,0)=p[r].y;
-			}
-		}
-		Eigen::MatrixXd c(degree+1,1);
-		c = ((m.transpose()*m).inverse())*(m.transpose()*y);
-		constants.clear();
-		for(int i=0;i<=degree;i++){
-			constants.push_back(c(i,0));
-			std::cout<<c(i,0)<<"\t";
-		}
-		std::cout<<std::endl;
-	}	
+
+struct ind{
+    int x; int y;
+    struct ind* next;
 };
 
-struct poly{
-	int degree;
-	float* coeff;
-};
-
-struct poly add(struct poly p1,struct poly p2){
-    int n=p1.degree,m=p2.degree;
-    int ld=n>m?m:n;
-    struct poly p3;
-    p3.degree=n+m-ld;
-    p3.coeff=new float[p3.degree+1];
-    for(int i=0;i<=ld;i++)p3.coeff[i]=p1.coeff[i]+p2.coeff[i];
-    for(int i=ld+1;i<=p3.degree;i++){
-        if(ld==n)p3.coeff[i]=p2.coeff[i];
-        else p3.coeff[i]=p1.coeff[i];}	
-    return p3;}
-
-struct poly mult(struct poly p1,struct poly p2){
-    struct poly p3;
-    p3.degree=p1.degree+p2.degree;
-    p3.coeff=new float[p3.degree+1];
-    for(int i=0;i<=p3.degree;i++)p3.coeff[i]=0;
-    for(int i=0;i<=p1.degree;i++){
-        for(int j=0;j<=p2.degree;j++)p3.coeff[i+j]+=p1.coeff[i]*p2.coeff[j];
-    }
-    return p3;
-}
-
-struct poly langrange(point* P){
-	struct poly p1,p2;
-	p1.degree=p2.degree=0;
-	p1.coeff=new float[1];
-	p2.coeff=new float[1];
-	p1.coeff[0]=1;
-	p2.coeff[0]=0;
-	for(int i=0;i<=5;i++){
-		struct poly p3;
-		p3.degree=1;
-		p3.coeff=new float[2];
-		p3.coeff[0]=1;
-		float denom=1;
-		for(int j=0;j<=5;j++){
-			if(j!=i){denom*=(P[i].x-P[j].x);
-				p3.coeff[1]=-P[j].x;
-				p1=mult(p1,p3);}
-		}
-		denom/=P[i].y;
-		struct poly pt;
-		pt.degree=0;
-		pt.coeff=new float[1];
-		if(denom!=0)pt.coeff[0]=1/denom;
-		else pt.coeff[0]=0;
-		p1=mult(p1,pt);
-		p2=add(p2,p1);
-		p1.degree=0;
-		p1.coeff=new float[1];
-		p1.coeff[0]=1;
-		//std::cout<<"degree so far:"<<p2.degree<<" ";
-	}
-return p2;
-}
-
-struct ind{int x;int y;
-struct ind* next;};
 struct ind *front,*rear;
 
 
-void equation(int mark,point* min_x,point* min_y,point* max_x,point* max_y,int* connected_index,struct poly* p_lane1,struct poly* p_lane2);
 cv::Mat game_binary(cv::Mat img);
 void enqueue(int x,int y);
 void dequeue();
@@ -156,13 +71,13 @@ int qempty();
 cv::Mat qblobdetect(cv::Mat& bin,int** A,int* mark);
 cv::Mat game_binary(cv::Mat img);
 cv::Mat link_blobs(cv::Mat qblob,int** A,int mark,point* min_x,point* max_x,point* min_y,point* max_y,int* connected_index);
-void equation(int mark,point* min_x,point* min_y,point* max_x,point* max_y,int* connected_index,struct poly* p_lane1,struct poly* p_lane2);
 
 cv::Mat LaneDetector::SeperateLanes(cv::Mat &image){
 
     image=game_binary(image);
 
     int mark=0;
+
     int**A=new int*[image.rows];
 
     for(int i=0;i<image.rows;i++)A[i] = new int[image.cols];
@@ -171,7 +86,7 @@ cv::Mat LaneDetector::SeperateLanes(cv::Mat &image){
         for(int j=0;j<image.cols;j++)A[i][j]=-1;
     }
 
-    cv::Mat qblob = qblobdetect(image,A, &mark);
+    cv::Mat qblob = qblobdetect(image, A, &mark);
 
     point* min_x = new point[mark+1];
     point* max_x = new point[mark+1];
@@ -188,39 +103,9 @@ cv::Mat LaneDetector::SeperateLanes(cv::Mat &image){
     int* connected_index = new int[mark+1];
 
     for(int i=0;i<=mark;i++) connected_index[i]=i;
+
     cv::Mat linked_image = link_blobs(qblob,A,mark,min_x,max_x,min_y,max_y,connected_index);
 
-    std::vector<point> max_x_v,min_x_v,max_y_v,min_y_v;
-    for(int i=1;i<=mark;i++){
-        max_x_v.push_back(max_x[i]);
-        min_x_v.push_back(min_x[i]);
-        min_y_v.push_back(min_y[i]);
-        max_y_v.push_back(max_y[i]);
-    }
-
-    Curve_C cu;
-    cu.make_curve(max_x_v,5);
-
-    point* P=new point[6];
-
-    for(int i=0;i<=5;i++){
-        P[i].x=i;
-        P[i].y=i*i+2*i-1;
-    }
-
-    for(int i=0;i<=cu.degree;i++) std::cout<<cu.constants.at(i)<<" ";
-    std::cout<<std::endl;
-
-    cv::Mat func(image.rows,image.cols,CV_8UC3);
-
-    for(int i=0;i<image.rows;i++){
-        int j = cu.constants.at(0)+i*(cu.constants.at(1)+i*(cu.constants.at(2)+i*(cu.constants.at(3)+i*(cu.constants.at(4)+i*cu.constants.at(5)))))/1;
-        if(j>=0 && j<image.cols) cv::circle(func,cv::Point(i,j),4,cv::Scalar(255,0,0),-1);
-    }
-
-    // cv::namedWindow("function",CV_WINDOW_AUTOSIZE);
-    // cv::imshow("function",func);
-    // cv::waitKey(0);
     return linked_image;
 }
 
@@ -258,9 +143,10 @@ int qempty(){
 
 
 cv::Mat qblobdetect(cv::Mat& bin,int** A,int* mark){
-    //std::cout<<"hello";
-    int i,j,x,y,wd=bin.cols,ht=bin.rows,max_pix_count=0,max_size_index=0;
-    //cout<<" a->"<<A[10][10]<<" "<<mark<<endl;
+    int i,j,x,y,wd=bin.cols,ht=bin.rows;
+
+    std::queue<std::pair<int, int> > Q;
+
     for(i=0;i<ht;i++){
         for(j=0;j<wd;j++){
             if(bin.at<uchar>(i,j)==255){
@@ -268,7 +154,6 @@ cv::Mat qblobdetect(cv::Mat& bin,int** A,int* mark){
                     *mark=*mark+1;
                     enqueue(i,j);
 
-                    //A[i][j]=0;
                     while(qempty()==0){
                         qfront(&x,&y);
                         int k,l;
@@ -321,8 +206,6 @@ cv::Mat game_binary(cv::Mat img){
     return bin;
 }
 
-
-
 cv::Mat link_blobs(cv::Mat qblob,int** A,int mark,point* min_x,point* max_x,point* min_y,point* max_y,int* connected_index){
     for(int i=0;i<qblob.rows;i++){
         for(int j=0;j<qblob.cols;j++){ int r=A[i][j];
@@ -361,71 +244,3 @@ cv::Mat link_blobs(cv::Mat qblob,int** A,int mark,point* min_x,point* max_x,poin
     return linked;
 
 }
-
-void equation(int mark,point* min_x,point* min_y,point* max_x,point* max_y,int* connected_index,struct poly* p_lane1,struct poly* p_lane2){
-    int* connected_index2=new int[mark+1];
-    struct poly p1,p2,p3,p4;
-    point* max_x_lane_eq,*min_x_lane_eq,*min_y_lane_eq,*max_y_lane_eq;
-    max_x_lane_eq=new point[6];
-    max_y_lane_eq=new point[6];
-    min_x_lane_eq=new point[6];
-    min_y_lane_eq=new point[6];
-    for(int i=0;i<=mark;i++)connected_index2[i]=connected_index[i];
-    int all_zero=0;
-    do{int r;
-        all_zero=0;
-        p1.degree=p2.degree=p3.degree=p4.degree=0;
-        p1.coeff=new float[1];
-        p2.coeff=new float[1];
-        p3.coeff=new float[1];
-        p4.coeff=new float[1];
-        p1.coeff[0]=p2.coeff[0]=p3.coeff[0]=p4.coeff[0]=0;
-        for(int i=0;i<=mark;i++)if(connected_index2[i]!=0){all_zero=1;r=connected_index2[i];break;}
-
-        if(all_zero){int lane_pt=0,interpolated=0;
-            for(int i=0;i<=mark;i++){
-                if(connected_index2[i]==r){
-                    lane_pt++;
-                    max_x_lane_eq[lane_pt%6]=max_x[i];
-                    max_y_lane_eq[lane_pt%6]=max_y[i];
-                    min_x_lane_eq[lane_pt%6]=min_x[i];
-                    min_y_lane_eq[lane_pt%6]=min_y[i];
-                    if(lane_pt==6){
-                        for(int j=0;j<=5;j++)std::cout<<max_x_lane_eq[j].x<<" "<<max_x_lane_eq[j].y<<std::endl;
-                    }
-                    if(lane_pt>=6){interpolated++;
-
-                        p1=add(p1,langrange(max_x_lane_eq));
-                        p2=add(p2,langrange(max_y_lane_eq));
-                        p3=add(p3,langrange(min_x_lane_eq));
-                        p4=add(p4,langrange(min_y_lane_eq));
-                    }
-                    connected_index2[i]=0;}
-                //std::cout<<"done interpolating till "<<i<<" "<<lane_pt<<std::endl;
-
-            }
-
-            struct poly pt;
-            pt.degree=0;
-            pt.coeff=new float[1];
-            pt.coeff[0]=1.0/interpolated;
-            std::cout<<"1/interpolated="<<pt.coeff[0]<<std::endl;
-            p1=mult(p1,pt);p2=mult(p2,pt);p3=mult(p3,pt);p4=mult(p4,pt);
-            if(r==1){*p_lane1=add(p1,add(p2,add(p3,p4)));
-
-                pt.coeff[0]=0.25;
-                *p_lane1=mult(*p_lane1,pt);
-            }
-            else{*p_lane2=add(p1,add(p2,add(p3,p4)));
-
-                pt.coeff[0]=0.25;
-                *p_lane2=mult(*p_lane2,pt);
-            }
-        }
-    }while(all_zero!=0);
-    free(connected_index2);
-    free(max_x_lane_eq);
-    free(max_y_lane_eq);
-    free(min_x_lane_eq);
-    free(min_y_lane_eq);
-} 
