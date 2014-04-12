626c1ceb405cc1a9735c2e30af1aabeb4567610e
Kumar Krishna Agarwal <kumar.1994.14@gmail.com>
2014-04-12 21:06:28 +0530

    Working

--- planning/motion_planning/local_planner/src/a_star_seed/state_of_car.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  state_of_car.cpp
-//  AStarSeed
-//
-//  Created by Satya Prakash on 27/03/14.
-//  Copyright (c) 2014 Satya Prakash. All rights reserved.
-//
-
-#include "a_star_seed/state_of_car.hpp"
