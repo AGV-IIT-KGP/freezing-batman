e4fb44ae949fbb18fc6c2b60422a39dacbb8acc7
Kumar Krishna Agarwal <kumar.1994.14@gmail.com>
2014-04-12 20:58:17 +0530

    All working

--- robot/pilots/eklavya3_Pilot/src/main.cpp
+++ robot/pilots/eklavya3_Pilot/src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc,char* argv[]) {
 
     ros::NodeHandle nh; // nodeHandle
 
-    pub_control = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10); //Publisher for Control
+    pub_control = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10); //Publisher for Control
 
     ros::Subscriber sub_path = nh.subscribe("/path",10, sendCommand); //Subscriber for Path
 
