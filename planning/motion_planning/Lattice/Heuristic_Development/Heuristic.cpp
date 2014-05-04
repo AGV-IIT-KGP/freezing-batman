27d885a7d4627310b21607c1bd7a43fc4f12f0b6
abinashmeher999 <abinashdakshana999@gmail.com>
2014-05-04 23:38:35 +0530

    Started Developing Lattice.

---
+++ planning/motion_planning/Lattice/Heuristic_Development/Heuristic.cpp
@@ -0,0 +1,6 @@
+#include "Heuristic.hpp"
+
+double Heuristic::hCostDT(const LState& startState_,const LState& endState_){}
+double Heuristic::hCostPathLength(const LState& startState_,const LState& endState_){}
+double Heuristic::hCostTurnAngle(const LState& startState_,const LState& endState_){}
+double Heuristic::heuristicNormalise(double h_cost_dt_,double h_cost_pl_,double h_cost_ta_){}
