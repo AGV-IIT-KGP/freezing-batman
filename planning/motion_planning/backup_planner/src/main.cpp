ceff2518cf3948d5ab6d4c8f099223495454bf1b
aditya <narayanaditya95@gmail.com>
2014-05-11 15:49:35 +0530

    created main

---
+++ planning/motion_planning/backup_planner/src/main.cpp
@@ -0,0 +1,16 @@
+#include "backup_planner.hpp"
+
+int main(int argc,char* argv[]) {
+
+    const std::string node_name = "backup_planner";
+
+    ros::init(argc, argv, node_name.c_str());
+
+    ros::NodeHandle nh;
+    
+    //navigation::LocalPlanner local_planner_seed(nh);
+
+    //local_planner_seed.plan();
+
+    return 0;
+}
