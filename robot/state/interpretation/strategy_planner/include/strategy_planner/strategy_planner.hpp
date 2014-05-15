22a76a116ae261c7d0511f2ab5671eda72173873
abinashmeher999 <abinashdakshana999@gmail.com>
2014-05-16 02:53:23 +0530

    Made strategy_planner package and node but without the algorithm.

--- robot/state/interpretation/strategy_planner/include/strategy_planner/strategy_planner.hpp
+++ robot/state/interpretation/strategy_planner/include/strategy_planner/strategy_planner.hpp
@@ -1,36 +1,78 @@
-#include <iostream>
-#include <geometry_msgs/Pose.h>
-#include <ros/ros.h>
-#include <iostream>
-#include <cstdlib>
-#include <signal.h>
-#include <message_filters/subscriber.h>
+#ifndef STRATEGY_PLANNER
+#define STRATEGY_PLANNER
+
 #include <vector>
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
-#include "geometry_msgs/Pose.h"
-#include <boost/bind.hpp>
+#include <ros/ros.h>
+#include <geometry_msgs/Pose2D.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/String.h>
+#include <iosfwd>
+
+static const int BUFFER_SIZE = 10;
+static const int LOOP_RATE = 10;
 
 enum Navigators {
-	Dummy_Navigator    	= 0,
-	Nose_Navigator     	= 1,
-    Waypoint_Navigator 	= 2,
-    Lane_Navigator     	= 3,
+    Dummy_Navigator = 0,
+    Nose_Navigator = 1,
+    Waypoint_Navigator = 2,
+    Lane_Navigator = 3,
 };
 
 enum Planners {
-	A_Star_Seed 	= 0,
-	Quick_Response 	= 1,
+    A_Star_Seed = 0,
+    Quick_Response = 1,
 };
 
-geometry_msgs::Pose dummy_target_, nose_target_, waypoint_target_, lane_target_;
+class Strategy_Planner {
+    geometry_msgs::Pose2D dummy_target_, nose_target_, waypoint_target_, lane_target_;
+    geometry_msgs::Pose2D final_target;
+    bool is_high_priority;
+    bool has_target_reached;
+    std::string high_priority_status;
+    std::string which_planner_;
+    int Navigators, Planners;
 
-void setDummyTarget(geometry_msgs proposed_dummy_target_);
-void setNoseTarget(geometry_msgs proposed_nose_target_);
-void setWaypointTarget(geometry_msgs proposed_waypoint_target_);
-void setLaneTarget(geometry_msgs proposed_lane_target_);
+public:
+    void strategise();
+    void setHighPriority(std_msgs::String status);
+    void setFinalTarget(geometry_msgs::Pose2D set_target_);
+    void setWhichPlanner(std::string planner_);
 
-inline geometry_msgs::Pose getDummyTarget() 	const { return dummy_target_; }
-inline geometry_msgs::Pose getNoseTarget() 		const { return nose_target_; }
-inline geometry_msgs::Pose getWaypointTarget() 	const { return waypoint_target_; }
-inline geometry_msgs::Pose getLaneTarget() 		const { return lane_target_; }
+    void setDummyTarget(geometry_msgs::Pose2D proposed_dummy_target_);
+    void setNoseTarget(geometry_msgs::Pose2D proposed_nose_target_);
+    void setWaypointTarget(geometry_msgs::Pose2D proposed_waypoint_target_);
+    void setLaneTarget(geometry_msgs::Pose2D proposed_lane_target_);
+    void setNavigator(int navigator_);
+    void setPlanner(int planner_);
+    void checkifTargetReached();
+    std_msgs::Bool hasTargetReached();
+
+    inline geometry_msgs::Pose2D getDummyTarget() const {
+        return dummy_target_;
+    }
+
+    inline geometry_msgs::Pose2D getNoseTarget() const {
+        return nose_target_;
+    }
+
+    inline geometry_msgs::Pose2D getWaypointTarget() const {
+        return waypoint_target_;
+    }
+
+    inline geometry_msgs::Pose2D getLaneTarget() const {
+        return lane_target_;
+    }
+
+    inline geometry_msgs::Pose2D getFinalTarget() const {
+        return final_target;
+    }
+
+    inline std_msgs::String getWhichPlanner() const {
+        std_msgs::String pub_str;
+        pub_str.data = which_planner_;
+        return pub_str;
+    }
+};
+#endif
--- robot/state/interpretation/strategy_planner/src/strategy_planner.cpp
+++ robot/state/interpretation/strategy_planner/src/strategy_planner.cpp
@@ -1,25 +1,79 @@
-#include "strategy_planner/strategy_planner.hpp"
+#include <strategy_planner/strategy_planner.hpp>
 
-void setDummyTarget (geometry::msgs proposed_dummy_target_)
-{
-	dummy_target_ = proposed_dummy_target_;
-	return;
+void Strategy_Planner::setDummyTarget(geometry_msgs::Pose2D proposed_dummy_target_) {
+    dummy_target_ = proposed_dummy_target_;
 }
 
-void setNoseTarget (geometry::msgs proposed_nose_target_)
-{
-	nose_target_ = proposed_nose_target_;
-	return;
+void Strategy_Planner::setNoseTarget(geometry_msgs::Pose2D proposed_nose_target_) {
+    nose_target_ = proposed_nose_target_;
 }
 
-void setWaypointTarget (geometry::msgs proposed_waypoint_target_)
-{
-	waypoint_target_ = proposed_waypoint_target_;
-	return;
+void Strategy_Planner::setWaypointTarget(geometry_msgs::Pose2D proposed_waypoint_target_) {
+    waypoint_target_ = proposed_waypoint_target_;
 }
 
-void setLaneTarget (geometry::msgs proposed_lane_target_)
-{
-	lane_target_ = proposed_lane_target_;
-	return;
+void Strategy_Planner::setLaneTarget(geometry_msgs::Pose2D proposed_lane_target_) {
+    lane_target_ = proposed_lane_target_;
+}
+
+void Strategy_Planner::setHighPriority(std_msgs::String status) {
+    is_high_priority = true;
+    high_priority_status = (status.data);
+}
+
+void Strategy_Planner::setFinalTarget(geometry_msgs::Pose2D set_target_) {
+    final_target = set_target_;
+}
+
+void Strategy_Planner::setWhichPlanner(std::string planner_) {
+    which_planner_ = planner_;
+}
+
+void Strategy_Planner::setNavigator(int navigator_) {
+    Navigators = navigator_; //see enum for int values
+}
+
+void Strategy_Planner::setPlanner(int planner_) {
+    Planners = planner_; //see enum for int values
+}
+
+void Strategy_Planner::checkifTargetReached() {
+    if (high_priority_status == std::string("TARGET FOUND")) {
+        has_target_reached = true;
+    } else has_target_reached = false;
+}
+
+std_msgs::Bool Strategy_Planner::hasTargetReached() {
+    std_msgs::Bool pub_bool;
+    pub_bool.data = has_target_reached;
+    return pub_bool;
+}
+
+void Strategy_Planner::strategise() {
+    checkifTargetReached();
+    if (!is_high_priority) {
+        switch (Navigators) {
+            case Dummy_Navigator:
+                Strategy_Planner::setFinalTarget(dummy_target_);
+                break;
+            case Nose_Navigator:
+                Strategy_Planner::setFinalTarget(nose_target_);
+                break;
+            case Waypoint_Navigator:
+                Strategy_Planner::setFinalTarget(waypoint_target_);
+                break;
+            case Lane_Navigator:
+                Strategy_Planner::setFinalTarget(lane_target_);
+                break;
+        }
+
+        switch (Planners) {
+            case A_Star_Seed:
+                setWhichPlanner("A_Star_Seed");
+                break;
+            case Quick_Response:
+                setWhichPlanner("Quick_Response");
+                break;
+        }
+    }
 }
