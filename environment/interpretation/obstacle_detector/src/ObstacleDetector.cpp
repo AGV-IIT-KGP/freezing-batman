2684a8d7855c3cc764c5128ffba155a297c4eea6
Samuel Anudeep <banmido69@gmail.com>
2014-05-16 14:52:23 +0530

    1. Formatted obstacle_detector pkg according to NetBeans style 2. Introduced roslaunch and rosparam

--- environment/interpretation/obstacle_detector/src/ObstacleDetector.cpp
+++ environment/interpretation/obstacle_detector/src/ObstacleDetector.cpp
@@ -1,128 +1,113 @@
-#include "../include/ObstacleDetector.hpp"
-#include <climits>
 #include <cassert>
+#include <climits>
+#include <ObstacleDetector.hpp>
+#include <opencv2/highgui/highgui_c.h>
 
-void exit_with_help() {
-    std::cout <<
-            "Usage: lane-detector [options]\n"
-            "options:\n"
-            "-d  : Non-zero for debug\n"
-            "-s  : Subscriber topic name\n"
-            "-p  : Publisher topic name\n"
-            "-l  : Maximum distance we need \n"
-            "-m  : Mininum distance we need \n"
-            ;
-    exit(1);
-}
 void ObstacleDetector::publishData() {
     cv_bridge::CvImage out_msg;
     out_msg.encoding = sensor_msgs::image_encodings::MONO8;
-    out_msg.image = img;
+    out_msg.image = obstacle_map;
     out_msg.encoding = sensor_msgs::image_encodings::MONO8;
-    pub.publish(out_msg.toImageMsg());
+    obstacle_publisher.publish(out_msg.toImageMsg());
 }
+
 void ObstacleDetector::interpret() {
-    if (debug) {
-        cvNamedWindow("Control Box", 1);
+    if (debug_mode > 0) {
+        cv::imshow(std::string("/") + node_name + std::string("/raw_scan"), obstacle_map);
+        cv::waitKey(wait_time);
     }
-    int s = 5;
-    if (debug) {
-        cvCreateTrackbar("Kernel 1", "Control Box", &s, 20);
-        cv::namedWindow("Raw Scan", 0);
-        cv::imshow("Raw Scan", img);
-        cv::waitKey(WAIT_TIME);
-    }
-    int dilation_size = EXPAND_OBS;
+
+    int dilation_size = obstacle_expansion;
     cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE,
-            cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
-            cv::Point(dilation_size, dilation_size));
-    cv::dilate(img, img, element);
-    if (debug) {
-        cv::namedWindow("Dilate Filter", 1);
-        cv::imshow("Dilate Filter", img);
-        cv::waitKey(WAIT_TIME);
-    }
+                                                cv::Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+                                                cv::Point(dilation_size, dilation_size));
+    cv::dilate(obstacle_map, obstacle_map, element);
     publishData();
+
+    if (debug_mode > 0) {
+        cv::imshow(std::string("/") + node_name + std::string("/dilate_filter"), obstacle_map);
+        cv::waitKey(wait_time);
+    }
 }
+
 void ObstacleDetector::scanCallback(const sensor_msgs::LaserScan& scan) {
-    if(debug)ROS_INFO("Scan callback called");
     size_t size = scan.ranges.size();
     float angle = scan.angle_min;
     float maxRangeForContainer = scan.range_max - 0.1f;
-    img = img - img; // Assign zero to all pixels
+    obstacle_map = obstacle_map - obstacle_map; // Assign zero to all pixels
+
     for (size_t i = 0; i < size; ++i) {
         float dist = scan.ranges[i];
         if ((dist > scan.range_min) && (dist < maxRangeForContainer)) {
             double x1 = -1 * sin(angle) * dist;
             double y1 = cos(angle) * dist;
-            int x = (int) ((x1 * 100) + CENTERX);
-            int y = (int) ((y1 * 100) + CENTERY + LIDAR_Y_SHIFT);
-            if (x >= 0 && y >= min_dist && (int) x < MAP_MAX && (int) y < max_dist) {
+            int x = (int) ((x1 * 100) + center_x);
+            int y = (int) ((y1 * 100) + center_y + lidar_y_shift);
+            if (x >= 0 && y >= min_dist && (int) x < map_size && (int) y < max_dist) {
                 int x2 = (x);
-                int y2 = (MAP_MAX - y - 30 - 1);
-                if (!(y2 >= 0 && y2 < MAP_MAX)) {
+                int y2 = (map_size - y - 30 - 1);
+                if (!(y2 >= 0 && y2 < map_size)) {
                     continue;
                 }
-                img.at<uchar>(y2, x2) = 255;
+                obstacle_map.at<uchar>(y2, x2) = 255;
             }
         }
         angle += scan.angle_increment;
     }
+
     interpret();
 }
-ObstacleDetector::ObstacleDetector(int argc, char *argv[], ros::NodeHandle &node_handle) : nh(node_handle) {
-    topic_name = std::string("/obstacle_detector/obstacles");
-    sub_topic_name = std::string("/scan");
-    min_dist = 0;
-    max_dist = 400;
-    debug = 0;
-    for (int i = 1; i < argc; i++) {
-        if (argv[i][0] != '-') {
-            break;
-        }
-        if (++i >= argc) {
-        }
-        switch (argv[i - 1][1]) {
-            case 'd':
-                debug = atoi(argv[i]);
-                break;
-            case 's':
-                sub_topic_name = std::string(argv[i]);
-                break;
-            case 'p':
-                topic_name = std::string(argv[i]);
-                break;
-            case 'l':
-                max_dist = atoi(argv[i]);
-                break;
-            case 'm':
-                min_dist = atoi(argv[i]);
-                break;
-            default:
-                fprintf(stderr, "Unknown option: -%c\n", argv[i - 1][1]);
-                exit_with_help();
-        }
+
+ObstacleDetector::ObstacleDetector(std::string node_name, ros::NodeHandle& node_handle) {
+    loadParams(node_handle);
+    this->node_name = node_name;
+    
+    if (debug_mode > 0) {
+        cv::namedWindow(std::string("/") + node_name + std::string("/raw_scan"), CV_WINDOW_AUTOSIZE);
+        cv::namedWindow(std::string("/") + node_name + std::string("/dilate_filter"), CV_WINDOW_AUTOSIZE);
     }
-    it = new image_transport::ImageTransport(nh);
-    sub = nh.subscribe(sub_topic_name.c_str(), 2, &ObstacleDetector::scanCallback, this);
-    img = cv::Mat(MAP_MAX, MAP_MAX, CV_8UC1, cvScalarAll(0));
-    pub = it->advertise(topic_name.c_str(), 10);
+
+    obstacle_map = cv::Mat(map_size, map_size, CV_8UC1, cvScalarAll(0));
+    image_transport = new image_transport::ImageTransport(node_handle);
+    obstacle_publisher = image_transport->advertise(std::string("/") + node_name + std::string("/obstacles"), 10);
+    scan_subscriber = node_handle.subscribe("/scan", 2, &ObstacleDetector::scanCallback, this);
 }
+
 ObstacleDetector::~ObstacleDetector() {
 }
 
-int main(int argc, char** argv) {
-    std::string node_name;
-    ros::init(argc, argv, node_name.c_str());
-    ros::NodeHandle nh;
-    ObstacleDetector obstacle_detector(argc, argv, nh);
+void ObstacleDetector::loadParams(ros::NodeHandle& node_handle) {
+    // Default Params used while debugging
+    center_x = 500;
+    center_y = 100;
+    debug_mode = 0;
+    hokuyo_scale = 100;
+    lidar_y_shift = 30;
+    map_size = 1000;
+    max_dist = 400;
+    min_dist = 0;
+    obstacle_expansion = 30;
+    wait_time = 100;
 
-    ros::Rate loop_rate(LOOP_RATE);
-    if(debug)ROS_INFO("Obstacle Detector Thread Started...");
-    while (ros::ok()) {
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
-    if(debug)ROS_INFO("Obstacle code exiting");
+    node_handle.getParam(std::string("/") + node_name + std::string("/center_x"), center_x);
+    node_handle.getParam(std::string("/") + node_name + std::string("/center_y"), center_y);
+    node_handle.getParam(std::string("/") + node_name + std::string("/debug_mode"), debug_mode);
+    node_handle.getParam(std::string("/") + node_name + std::string("/hokuyo_scale"), hokuyo_scale);
+    node_handle.getParam(std::string("/") + node_name + std::string("/lidar_y_shift"), lidar_y_shift);
+    node_handle.getParam(std::string("/") + node_name + std::string("/map_size"), map_size);
+    node_handle.getParam(std::string("/") + node_name + std::string("/max_dist"), max_dist);
+    node_handle.getParam(std::string("/") + node_name + std::string("/min_dist"), min_dist);
+    node_handle.getParam(std::string("/") + node_name + std::string("/obstacle_expansion"), obstacle_expansion);
+    node_handle.getParam(std::string("/") + node_name + std::string("/wait_time"), wait_time);
+}
+
+int main(int argc, char** argv) {
+    std::string node_name("obstacle_detector");
+    ros::init(argc, argv, node_name.c_str());
+    ros::NodeHandle node_handle;
+
+    ObstacleDetector obstacle_detector(node_name, node_handle);
+
+    ros::spin();
     return 0;
 }
