79586c7821b15948664f48caeaab7c7b7088c244
Samuel Anudeep <banmido69@gmail.com>
2014-05-17 22:12:36 +0530

    1. Formatted 2. Topic and node names

--- environment/sensing/vn_ins/include/vn_ins.hpp
+++ environment/sensing/vn_ins/include/vn_ins.hpp
@@ -1,24 +1,20 @@
 #ifndef VECTORNAV_HPP
 #define	VECTORNAV_HPP
 
-#include <environment/Sensor.hpp>
-#include <ros/ros.h>
-#include <iostream>
-#include <fstream>
-#include <math.h>
-#include <stdio.h>
 #include <unistd.h>
-
-
-
-#include <vectornav.h>
-
-#include <std_msgs/Float64.h>
+#include <cmath>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <ros/ros.h>
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Imu.h>
 #include <sensor_msgs/NavSatFix.h>
+#include <std_msgs/Float64.h>
 #include <tf/transform_datatypes.h>
+#include <environment/Sensor.hpp>
+#include <vectornav.h>
 
 class vectorNav : public environment::Sensor {
 public:
@@ -31,24 +27,26 @@ public:
     vectorNav(int argc, char** argv);
     vectorNav(const vectorNav& orig);
     virtual ~vectorNav();
+    
 private:
-	Vn200 vn200;
-	Vn100 vn100;
-	int BAUD_RATE;
-	std::string COM_PORT_vn100;
-	std::string COM_PORT_vn200;
-	ros::NodeHandle *node_handle;
-	std::string node_name;
-	std::string gps_topic_name, yaw_topic_name;
-	//std::string imu_topic_name, twist_topic_name
-	ros::Publisher gps_pub, yaw_pub;
-	//ros::Publisher imu_pub, twist_pub;
-	sensor_msgs::NavSatFix _gps;
+    Vn200 vn200;
+    Vn100 vn100;
+    int baud_rate;
+    std::string vn100_com_port;
+    std::string vn200_com_port;
+    ros::NodeHandle *node_handle;
+    std::string node_name;
+    std::string fix_topic_name, yaw_topic_name;
+    //std::string imu_topic_name, twist_topic_name
+    ros::Publisher fix_publisher, yaw_publisher;
+    //ros::Publisher imu_pub, twist_pub;
+    sensor_msgs::NavSatFix _gps;
     /*sensor_msgs::Imu _imu;
     geometry_msgs::Twist _twist;*/
     std_msgs::Float64 _yaw;
     btQuaternion tf_angles;
-	int message_queue_size;
+    int message_queue_size;
+    
     void initializeParameters();
     void initializeParameters(int argc, char** argv);
     void setupCommunications();
--- environment/sensing/vn_ins/src/vn_ins.cpp
+++ environment/sensing/vn_ins/src/vn_ins.cpp
@@ -1,113 +1,107 @@
-#include "vn_ins.hpp"
-#include "LifeCycle.hpp"
+#include <vn_ins.hpp>
+#include <LifeCycle.hpp>
 
 vectorNav::vectorNav(int argc, char** argv) : Sensor(argc, argv) {
-	if(argc >1){
-		initializeParameters(argc, argv);
-	}
-	else{
-		initializeParameters();
-	}
-	ros::init(argc, argv, node_name.c_str());
+    if (argc > 1) {
+        initializeParameters(argc, argv);
+    } else {
+        initializeParameters();
+    }
+    ros::init(argc, argv, node_name.c_str());
     node_handle = new ros::NodeHandle();
-	setupCommunications();
+    setupCommunications();
 }
 
 vectorNav::~vectorNav() {
 }
 
 bool vectorNav::connect() {
-	vn200_connect(&vn200, COM_PORT_vn200.c_str(), BAUD_RATE);
-	vn100_connect(&vn100, COM_PORT_vn100.c_str(), BAUD_RATE);
+    vn200_connect(&vn200, vn200_com_port.c_str(), baud_rate);
+    vn100_connect(&vn100, vn100_com_port.c_str(), baud_rate);
     return true;
 }
 
 bool vectorNav::disconnect() {
-	vn200_disconnect(&vn200);
-	vn100_disconnect(&vn100);
+    vn200_disconnect(&vn200);
+    vn100_disconnect(&vn100);
     return true;
 }
 
 bool vectorNav::fetch() {
-	unsigned short gpsWeek, status;
-	float yaw;
-	VnYpr vn100_attitude;
+    unsigned short gpsWeek, status;
+    float yaw;
+    VnYpr vn100_attitude;
     VnVector3 vn100_magnetic, vn100_acceleration, vn100_angular_rate;
     vn100_getYawPitchRollMagneticAccelerationAngularRate(&vn100, &vn100_attitude, &vn100_magnetic, &vn100_acceleration, &vn100_angular_rate);
     yaw = vn100_attitude.yaw;
-    
+
     unsigned char gpsFix, numberOfSatellites;
     float speedAccuracy, timeAccuracy, attitudeUncertainty, positionUncertainty, velocityUncertainty, temperature, pressure;
     double gpsTime, latitude, longitude, altitude;
     VnVector3 magnetic, acceleration, angularRate, ypr, latitudeLognitudeAltitude, nedVelocity, positionAccuracy;
-    
-	vn200_getGpsSolution(&vn200, &gpsTime, &gpsWeek, &gpsFix, &numberOfSatellites, &latitudeLognitudeAltitude, &nedVelocity, &positionAccuracy, &speedAccuracy, &timeAccuracy);
-    ROS_INFO("Triangulating from %d satellites",numberOfSatellites);
-  
-	
+
+    vn200_getGpsSolution(&vn200, &gpsTime, &gpsWeek, &gpsFix, &numberOfSatellites, &latitudeLognitudeAltitude, &nedVelocity, &positionAccuracy, &speedAccuracy, &timeAccuracy);
+    ROS_INFO("Triangulating from %d satellites", numberOfSatellites);
 
     _gps.latitude = latitudeLognitudeAltitude.c0;
     _gps.longitude = latitudeLognitudeAltitude.c1;
     _gps.altitude = latitudeLognitudeAltitude.c2;
-    
-    _yaw.data=yaw;
-    
-	return true;
+
+    _yaw.data = yaw;
+
+    return true;
 }
 
 void vectorNav::publish(int frame_id) {
-  
-    gps_pub.publish(_gps);
-    yaw_pub.publish(_yaw);
+    fix_publisher.publish(_gps);
+    yaw_publisher.publish(_yaw);
 }
 
 void vectorNav::initializeParameters() {
-    node_name = std::string("sensors_vectorNav_0");
-    gps_topic_name=std::string("sensors/gps/0");
-  
-    yaw_topic_name=std::string("sensors/yaw/0");
+    baud_rate = 115200;
     message_queue_size = 10;
-    COM_PORT_vn200 = std::string("/dev/serial/by-id/usb-FTDI_USB-RS232_Cable_FTUTUVO5-if00-port0");
-    COM_PORT_vn100 = std::string("/dev/serial/by-id/usb-FTDI_USB-RS232_Cable_FTVJUC0O-if00-port0");
-    BAUD_RATE= 115200;
+    node_name = std::string("vn_ins");
+    fix_topic_name = node_name + std::string("/fix");
+    yaw_topic_name = node_name + std::string("/yaw");
+    vn200_com_port = std::string("/dev/serial/by-id/usb-FTDI_USB-RS232_Cable_FTUTUVO5-if00-port0");
+    vn100_com_port = std::string("/dev/serial/by-id/usb-FTDI_USB-RS232_Cable_FTVJUC0O-if00-port0");
 }
 
 void vectorNav::initializeParameters(int argc, char** argv) {
-	node_name = std::string("sensors_vectorNav_") + std::string(argv[1]);
-    gps_topic_name=std::string("sensors/gps/") + std::string(argv[1]);
-  
-    yaw_topic_name=std::string("sensors/yaw/") + std::string(argv[1]);
-    COM_PORT_vn200 = std::string(argv[2]);
-    COM_PORT_vn100 = std::string(argv[3]);
+    baud_rate = 115200;
     message_queue_size = 10;
-    BAUD_RATE= 115200;
+    node_name = std::string("vn_ins") + std::string(argv[1]);
+    fix_topic_name = node_name + std::string(argv[1]) + std::string("/fix");
+    yaw_topic_name = node_name + std::string(argv[1]) + std::string("/yaw");
+    vn200_com_port = std::string(argv[2]);
+    vn100_com_port = std::string(argv[3]);
 }
 
 void vectorNav::setupCommunications() {
-	gps_pub = node_handle->advertise<sensor_msgs::NavSatFix>(gps_topic_name.c_str(), message_queue_size);
-	
-    yaw_pub = node_handle->advertise<geometry_msgs::Twist>(yaw_topic_name.c_str(), message_queue_size);
+    fix_publisher = node_handle->advertise<sensor_msgs::NavSatFix>(fix_topic_name.c_str(), message_queue_size);
+    yaw_publisher = node_handle->advertise<geometry_msgs::Twist>(yaw_topic_name.c_str(), message_queue_size);
 }
 
 int main(int argc, char** argv) {
-	if(argc>1 && argc <=3){
-		printf("Usage : <name> <id_no> <COM_PORT>\n");
-	}
-	double loop_rate = 10;
-	int frame_id;
-	frame_id = 0;
+    if (argc > 1 && argc <= 3) {
+        printf("Usage : <name> <id_no> <COM_PORT>\n");
+    }
+
+    double loop_rate = 10;
+    int frame_id;
+    frame_id = 0;
     vectorNav *vectornav = new vectorNav(argc, argv);
-	vectornav->connect();
-    
-    ROS_INFO("VectorNav succesfully connected. \n");
-	
-	ros::Rate rate_enforcer(loop_rate);
-	while (ros::ok()) {
+    vectornav->connect();
+    ROS_INFO("VectorNav successfully connected. \n");
+
+    ros::Rate rate_enforcer(loop_rate);
+    while (ros::ok()) {
         vectornav->fetch();
         vectornav->publish(frame_id);
         ros::spinOnce();
         rate_enforcer.sleep();
     }
-    vectornav->disconnect();
-return 0;
+    
+    vectornav->disconnect();    
+    return 0;
 }
