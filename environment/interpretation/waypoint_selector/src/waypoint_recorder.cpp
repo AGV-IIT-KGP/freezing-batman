473533d4f810bd4a1342981bfc8d63c8207752d0
abinashmeher999 <abinashdakshana999@gmail.com>
2014-05-23 05:52:40 +0530

    Added a fully functional waypoint recorder, modified the launch file to fix an error and uncommented the lines in vn_ins.cpp

--- environment/interpretation/waypoint_selector/src/waypoint_recorder.cpp
+++ environment/interpretation/waypoint_selector/src/waypoint_recorder.cpp
@@ -8,6 +8,7 @@
 #include <ios>
 #include <iosfwd>
 #include <fstream>
+#include <iomanip>
 
 static const int buffer_size = 10;
 static const int loop_rate_hz = 10;
@@ -20,20 +21,18 @@ void setCurrentGPS(sensor_msgs::NavSatFix sensor_gps) {
 
 int main(int argc, char* argv[]) {
     int file_count, i, no_of_waypoints, count;
-    char c;
-    float term_lat, term_long;
+    std::string c;
+    sensor_msgs::NavSatFix term, temp;
     std::string num_file, txtfile_add, file_num_str;
     std::fstream numfile, waypoints_file, edit_numfile;
-    std::vector<std::pair<float, float> > waypoints_vector;
-    std::pair<float, float> temp_pair;
+    std::vector<sensor_msgs::NavSatFix> waypoints_vector;
+    sensor_msgs::NavSatFix temp_pair;
 
     ros::init(argc, argv, "waypoint_recorder");
     ros::NodeHandle node_handle;
 
     node_handle.getParam("/waypoint_recorder/data_file", num_file);
     node_handle.getParam("/waypoint_recorder/record_file_folder", txtfile_add);
-//    std::cout << num_file << std::endl;
-//    std::cout << txtfile_add << std::endl;
 
     numfile.open(num_file.c_str());
 
@@ -49,35 +48,41 @@ int main(int argc, char* argv[]) {
     }
     std::cout << "Enter the no of waypoints:";
     std::cin>>no_of_waypoints;
-    std::cout << "ENTER to record an entry\n'e' to end\nand 'n'(only after 'e') to close file when finished taking waypoints and make a txt." << std::endl;
+    std::cin.ignore(1, '\n');
 
     ros::Subscriber sub_current_gps = node_handle.subscribe("/vn_ins/fix", buffer_size, setCurrentGPS);
 
     ros::Rate loop_rate(loop_rate_hz);
     count = 0;
     while (ros::ok() && count < no_of_waypoints) {
-        for (i = 0, term_lat = 0, term_long = 0;; i++) {
-            c = getchar();
-            if (c == '\n') {
-                std::cout<<"Reading "<<count+1<<"th waypoint"<<i+1<<"th time."<<std::endl;
+        ros::spinOnce();
+        for (i = 0, term.latitude = current_gps.latitude, term.longitude = current_gps.longitude; term.latitude != 0 && term.longitude != 0; ++i) {
+            std::cout << "ENTER to record an entry\n'e' to end\nand 'n'(only after 'e') to close file when finished taking waypoints and make a txt." << std::endl;
+            std::getline(std::cin, c);
+            if (c == "") {
+                std::cout << "Reading " << count + 1 << "th waypoint " << i + 1 << "th time." << std::endl;
                 ros::spinOnce();
-                term_lat += ((term_lat * i) + current_gps.latitude) / (i + 1);
-                term_long += ((term_long * i) + current_gps.longitude) / (i + 1);
-                temp_pair.first = term_lat;
-                temp_pair.second = term_long;
+                temp = current_gps;
+                std::cout << "latitude:" << std::setprecision(10) << temp.latitude << " " << "longitude:" << std::setprecision(10) << temp.longitude << std::endl;
+                term.latitude = (((term.latitude * i) + temp.latitude) / (i + 1));
+                term.longitude = (((term.longitude * i) + temp.longitude) / (i + 1));
+                temp_pair.latitude = term.latitude;
+                temp_pair.longitude = term.longitude;
                 continue;
-            } else if (c == 'e') {
+            } else if (c == "e") {
+                std::cout << "\n";
                 break;
-            } else if (c == 'n') {
+            } else if (c == "n") {
+                std::cout << "\n";
                 break;
             }
         }
-        if (c == 'e') {
+        if (c == "e") {
             waypoints_vector.push_back(temp_pair);
             count++;
-            std::cout<<"Read "<<count<<"th waypoint"<<std::endl;
-            std::cout<<"------------------------------------"<<std::endl;
-        } else if (c == 'n') {
+            std::cout << "Read " << count << "th waypoint\n Move to the next waypoint.\n" << std::endl;
+            std::cout << "------------------------------------" << std::endl;
+        } else if (c == "n") {
             break;
         }
     }
@@ -86,13 +91,13 @@ int main(int argc, char* argv[]) {
     edit_numfile.close();
 
     std::string record_file = (txtfile_add + "GPS_waypoints" + file_num_str + ".txt");
-    std::cout<<"Writing to "<<record_file<<std::endl;
-    waypoints_file.open(record_file.c_str(),std::ios::in | std::ios::out | std::ios::trunc);
+    std::cout << "Writing to " << record_file << std::endl;
+    waypoints_file.open(record_file.c_str(), std::ios::in | std::ios::out | std::ios::trunc);
     waypoints_file << count << std::endl;
-    for (std::vector < std::pair<float, float> >::iterator it = waypoints_vector.begin(); it != waypoints_vector.end(); it++) {
-        waypoints_file << it->first;
+    for (std::vector <sensor_msgs::NavSatFix>::iterator it = waypoints_vector.begin(); it != waypoints_vector.end(); it++) {
+        waypoints_file << std::setprecision(10) << it->latitude;
         waypoints_file << " ";
-        waypoints_file << it->second << std::endl;
+        waypoints_file << std::setprecision(10) << it->longitude << std::endl;
     }
     waypoints_file << "#mention this file in the launch of waypoint_selector." << std::endl;
     waypoints_file.close();
