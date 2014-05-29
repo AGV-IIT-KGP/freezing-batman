b81b9f8baf8ab3baf85ab11427482ff44301726a
Samuel Anudeep <banmido69@gmail.com>
2014-05-29 15:19:24 +0530

    qr_planner modified with distance transform.

--- planning/motion_planning/local_planner/src/a_star_seed/distance_transform.cpp
+++ planning/motion_planning/local_planner/src/a_star_seed/distance_transform.cpp
@@ -43,22 +43,20 @@ namespace navigation {
         cv::Mat binary_img, transformed_img;
         int i, j;
         int binary_min_threshold, binary_max_threshold;
+        float DtThresh = 200;
         node_handle.getParam("local_planner/max_threshold", binary_max_threshold);
         node_handle.getParam("local_planner/min_threshold", binary_min_threshold);
         cv::threshold(fusion_map, binary_img, 100, 255, CV_THRESH_BINARY);
 
         binary_img = 255 - binary_img;
         cv::distanceTransform(binary_img, transformed_img, CV_DIST_L2, 3);
-        //        float DtThresh = 100;
-        //        for (i = 0; i < fusion_map.rows; i++)
-        //            for (j = 0; j < fusion_map.cols; j++)
-        //                if (transformed_img.at<float>(i, j) > DtThresh)
-        //                    transformed_img.at<float>(i, j) = DtThresh;
+
+       
         cv::normalize(transformed_img, transformed_img, 0, 1, cv::NORM_MINMAX);
         double minVal, maxVal;
-        minMaxLoc(transformed_img, &minVal, &maxVal);
+        cv::minMaxLoc(transformed_img, &minVal, &maxVal);
         //binary_img.convertTo(binary_img, CV_8U, 400.0 / (maxVal - minVal), -minVal * 400.0 / (maxVal - minVal));
-        transformed_img.convertTo(binary_img, CV_8U, 800.0 / (maxVal - minVal), -minVal * 800.0 / (maxVal - minVal));
+        transformed_img.convertTo(binary_img, CV_8U, 255.0 / (maxVal - minVal), -minVal * 255.0 / (maxVal - minVal));
 
         binary_img = 255 - binary_img;
         cv::rectangle(binary_img, cv::Point(0 * binary_img.cols, 0 * binary_img.rows), cv::Point(.1 * binary_img.cols, 1 * binary_img.rows), cv::Scalar(0, 0, 0), CV_FILLED, 8, 0);
@@ -66,6 +64,14 @@ namespace navigation {
         cv::rectangle(binary_img, cv::Point(.9 * binary_img.cols, .9 * binary_img.rows), cv::Point(1 * binary_img.cols, 0 * binary_img.rows), cv::Scalar(0, 0, 0), CV_FILLED, 8, 0);
         cv::rectangle(binary_img, cv::Point(.1 * binary_img.cols, 0 * binary_img.rows), cv::Point(.9 * binary_img.cols, .1 * binary_img.rows), cv::Scalar(0, 0, 0), CV_FILLED, 8, 0);
         fusion_map = binary_img;
+//         for (i = 0; i < fusion_map.rows; i++)
+//            for (j = 0; j < fusion_map.cols; j++) {
+//                if (fusion_map.at<uchar>(i, j) < DtThresh) {
+//                    fusion_map.at<uchar>(i, j) = 0;
+//                } else {
+//                    fusion_map.at<uchar>(i, j) = (fusion_map.at<uchar>(i, j) - 205)*255 / 50;
+//                }
+//            }
 
         //cv::threshold(fusion_map, fusion_map, 245, 0, CV_THRESH_TOZERO);
     }
